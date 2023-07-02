531d9b0f6f5bdbc596876cb43140d5b13c2d0d69
wtlin1228 <wtlin1228@gmail.com>
2023-07-02 21:49:35 +0800

    add combo sections with access controler as examples

--- src/pages/Combo.tsx
+++ src/pages/Combo.tsx
@@ -1,4 +1,9 @@
+import { AccessController } from "../AccessControl/AccessController";
 import { useAccessControlContext } from "../AccessControl/context";
+
+const ComboSection: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <div className="combo-section">{children}</div>
+);
 
 export default function Combo() {
   const { accessControl } = useAccessControlContext();
@@ -7,6 +12,37 @@ export default function Combo() {
     <div className="page">
       <h1>Combo Page</h1>
       <h2>Access control: {accessControl}</h2>
+
+      <AccessController
+        rule={["core", "bp"]}
+        fallback={
+          <ComboSection>enable core & bp module to use this combo</ComboSection>
+        }
+      >
+        <ComboSection>Core & Branch Purchase Combo</ComboSection>
+      </AccessController>
+
+      <AccessController
+        rule={["core", "member"]}
+        fallback={
+          <ComboSection>
+            enable core & member module to use this combo
+          </ComboSection>
+        }
+      >
+        <ComboSection>Core & Member Combo</ComboSection>
+      </AccessController>
+
+      {/* <AccessController
+        rule={["core", "member", "bp"]}
+        fallback={
+          <ComboSection>
+            enable core & member & bp module to use this combo
+          </ComboSection>
+        }
+      >
+        <ComboSection>Core & Member & Branch Purchase Combo</ComboSection>
+      </AccessController> */}
     </div>
   );
 }
