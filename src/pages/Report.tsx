5071b9a29976c351c3ac214c8ea29917185d9d2b
wtlin1228 <wtlin1228@gmail.com>
2023-07-02 21:41:09 +0800

    add report sections

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,4 +1,9 @@
+import { AccessController } from "../AccessControl/AccessController";
 import { useAccessControlContext } from "../AccessControl/context";
+
+const ReportSection: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <div className="report-section">{children}</div>
+);
 
 export default function Report() {
   const { accessControl } = useAccessControlContext();
@@ -7,6 +12,33 @@ export default function Report() {
     <div className="page">
       <h1>Report Page</h1>
       <h2>Access control: {accessControl}</h2>
+
+      <AccessController
+        rule={["core"]}
+        fallback={
+          <ReportSection>enable core module to see core report</ReportSection>
+        }
+      >
+        <ReportSection>Core Report</ReportSection>
+      </AccessController>
+
+      <AccessController
+        rule={["member"]}
+        fallback={
+          <ReportSection>enable core module to see member report</ReportSection>
+        }
+      >
+        <ReportSection>Member Report</ReportSection>
+      </AccessController>
+
+      {/* <AccessController
+        rule={["bp"]}
+        fallback={
+          <ReportSection>enable core module to see bp report</ReportSection>
+        }
+      >
+        <ReportSection>Branch Purchase Report</ReportSection>
+      </AccessController> */}
     </div>
   );
 }
